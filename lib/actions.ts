e23b6f33bda5e3460a03ffae1448e547b9cf0713
Robert C <carderober@gmail.com>
2024-10-23 20:50:28 -0300

    upload image bucket ready

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,6 +5,7 @@ import { imageSchema, profileSchema, validateWithZodSchema } from './schemas';
 import { auth, clerkClient, currentUser, User } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { uploadImage } from './supabase';
 
 const getAuthUser = async (): Promise<User> => {
   const user = await currentUser();
@@ -106,8 +107,26 @@ export const updateProfileImageAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  const image = formData.get('image') as File;
-  const validateFields = validateWithZodSchema(imageSchema, { image });
+  const user = await getAuthUser();
 
-  return { message: 'Profile image updated successfully' };
+  try {
+    const image = formData.get('image') as File;
+    const validateFields = validateWithZodSchema(imageSchema, { image });
+    const fullPath = await uploadImage(validateFields.image);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: {
+        profileImage: fullPath,
+      },
+    });
+
+    revalidatePath('/profile');
+
+    return { message: 'Profile image updated successfully' };
+  } catch (error) {
+    return renderError(error);
+  }
 };
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'img.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'zrgiilcbdaxplrrjgskt.supabase.co',
       },
     ],
   },
