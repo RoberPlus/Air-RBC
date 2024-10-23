c6471cc50fef27fefaccfeebef5d77e95e317a32
Robert C <carderober@gmail.com>
2024-10-23 19:11:02 -0300

    Update profile action - ready

--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useFormState } from 'react-dom';
+import { useActionState } from 'react';
 import { useEffect } from 'react';
 import { useToast } from '@/hooks/use-toast';
 import { actionFunction } from '../../lib/types';
@@ -16,16 +16,16 @@ const FormContainer = ({
   action: actionFunction;
   children: React.ReactNode;
 }) => {
-  const [state, formAction] = useFormState(action, initialState);
+  const [state, formAction] = useActionState(action, initialState);
   const { toast } = useToast();
 
   useEffect(() => {
     if (state.message) {
-      toast({description: state.message})
+      toast({ description: state.message });
     }
-  }, [state])
+  }, [state]);
 
-  return <form action={formAction}>{children}</form>
+  return <form action={formAction}>{children}</form>;
 };
 
 export default FormContainer;
--- lib/actions.ts
+++ lib/actions.ts
@@ -2,15 +2,28 @@
 
 import db from './db';
 import { profileSchema } from './schemas';
-import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { auth, clerkClient, currentUser, User } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+
+const getAuthUser = async (): Promise<User> => {
+  const user = await currentUser();
+
+  if (!user) throw new Error('You must be logged in, please back to homepage');
+  if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+
+  return user;
+};
+
+const renderError = (error:unknown):{message:string}=>{
+  return { message: error instanceof Error ? error.message : 'An error occurred!' };
+}
 
 export const createProfileAction = async (prevState: any, formData: FormData) => {
   try {
     const user = await currentUser();
 
-    if (!user) throw new Error('No user found');
+    if (!user) throw new Error('You must be logged in, please back to homepage');
 
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
@@ -32,9 +45,59 @@ export const createProfileAction = async (prevState: any, formData: FormData) =>
       },
     });
   } catch (error) {
-    console.log(error);
-
-    return { message: error instanceof Error ? error.message : 'An error occurred!' };
+    return renderError(error)
   }
   redirect('/');
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+
+  return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!profile) redirect('/profile/create');
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+
+    revalidatePath('/profile');
+    return { message: 'Profile Updated!' };
+  } catch (error) {
+    return renderError(error)
+  }
 };
