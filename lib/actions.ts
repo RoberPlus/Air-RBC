08d6da134204be26cff94905cdbd74edc249333a
Robert C <carderober@gmail.com>
2024-10-23 17:58:03 -0300

    Create Profile action - db - ready

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,16 +1,40 @@
 'use server';
 
+import db from './db';
 import { profileSchema } from './schemas';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export const createProfileAction = async (prevState: any, formData: FormData) => {
   try {
+    const user = await currentUser();
+
+    if (!user) throw new Error('No user found');
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
 
-    return { message: 'Profile Created' };
+    await db.profile.create({
+      data: {
+        clerkId: user?.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+
+    const response = await clerkClient();
+
+    response.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
     console.log(error);
 
-    return { message: 'there was an error' };
+    return { message: error instanceof Error ? error.message : 'An error occurred!' };
   }
+  redirect('/');
 };
