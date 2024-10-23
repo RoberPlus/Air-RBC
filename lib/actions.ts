e5c6daaddbcda6953003c8d0615deb55016d983b
Robert C <carderober@gmail.com>
2024-10-23 17:02:39 -0300

    zod basic implement

---
+++ lib/actions.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import { profileSchema } from './schemas';
+
+export const createProfileAction = async (prevState: any, formData: FormData) => {
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+
+    return { message: 'Profile Created' };
+  } catch (error) {
+    console.log(error);
+
+    return { message: 'there was an error' };
+  }
+};
