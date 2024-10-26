17a2b1a8f94752933a427d44b2057d09125bd298
Robert C <carderober@gmail.com>
2024-10-26 20:01:30 -0300

    Rentals finished

--- components/form/AmenitiesInput.tsx
+++ components/form/AmenitiesInput.tsx
@@ -6,7 +6,15 @@ import { amenities } from '@/lib/amenities';
 import { Checkbox } from '@/components/ui/checkbox';
 
 const AmenitiesInput = ({ defaultValue }: { defaultValue?: Amenity[] }) => {
-  const [selectedAmenities, setSelectedAmenities] = useState<Amenity[]>(defaultValue || amenities);
+  const amenitiesWithIcons = defaultValue?.map(({name, selected})=>{
+    return{
+      name,
+      selected,
+      icon: amenities.find((amenity)=> amenity.name === name)!.icon
+    }
+  })
+
+  const [selectedAmenities, setSelectedAmenities] = useState<Amenity[]>(amenitiesWithIcons || amenities);
 
   const handleChange = (amenity: Amenity) => {
     setSelectedAmenities((prev) => {
